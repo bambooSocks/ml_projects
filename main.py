66d2fde20514ee60070a07f47f4fed7d354d9f2c
Matej <majtan.matej@gmail.com>
2020-09-27 18:07:56 +0200

    loaded data

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-data = pd.read_csv("heart.csv")
+df = pd.read_csv("heart.csv")
+cols = np.asarray(df.columns)
 
+data = df.to_numpy()
+
+print(cols)
 print(data)
+
+thal = np.transpose(data)[12]
+
+plt.hist(thal, bins=4, rwidth=0.75, color=['red', 'blue', 'green', 'yellow'])
+plt.show()
