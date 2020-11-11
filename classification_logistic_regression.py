b90f6fbb05f7ea899d9a2f0d537dd0e800690d85
Teodora <bejenariut@yahoo.com>
2020-11-11 11:50:39 +0100

    logistic regression - updated

--- classification_logistic_regression.py
+++ classification_logistic_regression.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from toolbox_02450 import rocplot, confmatplot
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import RFE
 
 from data_aquisition import *
 
@@ -14,8 +15,27 @@ attributeNames = np.delete(sel_attr, -1, axis=0)
 
 X = np.delete(X_sel, -1, axis=1)
 
+# We select use the RFE package to recursively select the features of most importance:
 
-# We treat angina as categorical. since we do not know which one is worse (typical or atypical)
+
+logreg = LogisticRegression(fit_intercept = False)
+logreg.fit(X,y)
+selector = RFE(logreg, n_features_to_select=1)
+selector = selector.fit(X, y)
+order = selector.ranking_
+print(order)
+
+feature_ranks = []
+for i in order:
+    feature_ranks.append(f"{i}. {attributeNames[i-1]}")
+print("Most important feature ranking which should be considered for a future model is:", feature_ranks)
+
+'''
+Most important features ranking: ['5. chol', '3. cp', '1. age', '7. oldpeak', '8. slope', '6. thalach', '2. sex', '4. trestbps']    
+However for this project we will use all attributes and use a regularization term to control complexity.
+Performance of different models (ANN, CT, KNN, NB) will be analyzed afterwards.
+'''
+# We treat thalach as categorical. since we do not know which angina pain type is worse (typical or atypical)
 
 # One out of hot encoding:
 enc = OneHotEncoder()
@@ -50,6 +70,7 @@ lambda_interval = np.logspace(-8, 5, 50)
 train_error_rate = np.zeros(len(lambda_interval))
 test_error_rate = np.zeros(len(lambda_interval))
 coefficient_norm = np.zeros(len(lambda_interval))
+coefficient_matrix = np.zeros((len(lambda_interval),15))
 for k in range(0, len(lambda_interval)):
     #regularization regression - L2
     mdl = LogisticRegression(penalty='l2', C=1/lambda_interval[k] )
@@ -65,10 +86,18 @@ for k in range(0, len(lambda_interval)):
 # magnitude of the coefficients
     w_est = mdl.coef_[0] 
     coefficient_norm[k] = np.sqrt(np.sum(w_est**2))
+    coef_vector = np.concatenate((mdl.intercept_,np.squeeze(mdl.coef_)))
+    coefficient_matrix[k,:] = coef_vector
 
 min_error = np.min(test_error_rate)
-opt_lambda_idx = np.argmin(test_error_rate)
+opt_lambda_idx = np.argmin(test_error_rate) #26
+opt_wieghts = coefficient_matrix[26,:]
+#  array([ 0.99453237,  0.72318361,  0.68650974,  0.61346421,  1.58205608,
+#       -2.04352359,  1.92511527, -1.92511527, -1.2582287 ,  0.00290383,
+#       -0.97304672,  2.50443067, -0.01087359, -0.28967932,  0.29834006])
 opt_lambda = lambda_interval[opt_lambda_idx]
+
+# 0.07906043210907701
 
 plt.figure(figsize=(8,8))
 plt.semilogx(lambda_interval, train_error_rate*100)
@@ -92,3 +121,11 @@ plt.xlabel('Regularization strength, $\log_{10}(\lambda)$')
 plt.title('Parameter vector L2 norm')
 plt.grid()
 plt.show()    
+
+'''
+Note: 
+As one can see, the minimum test error is 26.49% which is quite high.
+This is due to the fact that we have used all the attributes and only the regularization term for controlling model complexity. 
+This means our model will classify y correctly about 1/4 times. 
+
+'''
